17d6082f42e53921439bd57ab0a26efcec6e1f6e
Abigail W. Berg <X00333964@wcc.dom>
2016-04-22 11:06:43 -0500

    Adding week 12 file

---
+++ CIS115-CH8-3.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Date Printer
+#-------------------------------------------------------------------------------
+
+def main():
+
+    date = input('Enter date in form of mm/dd/yyyy')
+    month = date[0:2]
+    day = date[3:5]
+    year = date [6:]
+
+    transmonth = translate(month)
+
+    print (transmonth, day, sep= ',', year)
+
+def translate(month):
+    if month == '01':
+        month = 'January'
+    elif month == '02':
+        month = 'February'
+    elif month == '03':
+        month = 'March'
+    elif month == '04':
+        month = 'April'
+    elif month == '05':
+        month = 'May'
+    elif month == '06':
+        month = 'June'
+    elif month == '07':
+        month = 'July'
+    elif month == '08':
+        month = 'August'
+    elif month == '09':
+        month = 'September'
+    elif month == '10':
+        month = 'October'
+    elif month == '11':
+        month = 'November'
+    else:
+        month = 'December'
+
+    return month
+main()
